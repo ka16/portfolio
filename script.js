e890d388f8383f703d8dfd9905e89cc4d0a25dcf
ka16 <69737968+ka16@users.noreply.github.com>
2020-11-29 11:34:51 +0900

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,11 @@ homeBtn.click(function () {
   }, 500);
   return false;
 });
+
+$('header a').click(function() {
+    var id = $(this).attr('href');
+    var position = $(id).offset().top;
+    $('html, body').animate({            
+      'scrollTop': position             
+    }, 500);
+  });
